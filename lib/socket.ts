7b73f574b0e15bbd2ddf65a13c6abcb68b3fa3d3
Alireza <dot.alireza.0@gmail.com>
2026-08-19 00:35:10 +0330

    chore: changed socket.io url

--- lib/socket.ts
+++ lib/socket.ts
@@ -8,10 +8,10 @@ function createSocket() {
     const directApiUrl = process.env.NEXT_PUBLIC_API_URL
 
     if (directApiUrl) {
-        const url = new URL(directApiUrl)
-        url.hostname = window.location.hostname
+        // const url = new URL(directApiUrl)
+        // url.hostname = window.location.hostname
 
-        return io(url.origin, {
+        return io("https://pulse-chat-api-5fez.onrender.com", {
             withCredentials: true,
             autoConnect: false,
         })
